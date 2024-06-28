ba3c10e2ffa27fe3029444c881b5ecb3bde652d2
JAVIER CANTEROS <ZIROCOOL2003@HOTMAIL.COM>
2024-06-28 20:30:54 -0300

    fix

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,8 +1,8 @@
 const config = {
-    host: '193.203.175.98',
-    user: 'u223632581_odontologia',
-    password: '>7HjT8FtVyx',
-    database: 'u223632581_codoacodobacke'
+    host: -,
+    user: -,
+    password: -,
+    database: -
 }
 
 export default config
